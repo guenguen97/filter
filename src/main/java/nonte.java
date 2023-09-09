ed9cb3d0c6261a3525b176d1521c2a7ad283c202
youngguen lee <[200~guenguen9797@gmail.comgit config --global user.emailbacke]|git config --global user.email [200~guenguen9797@gmail.com>
2023-09-09 21:07:11 +0900

    숫자 표기

---
+++ src/main/java/nonte.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class nonte {
+    public static void main(String[] args) {
+
+    }
+
+
+    class Solution {
+        public int solution(int[] sides) {
+            int answer = 0;
+
+            Arrays.sort(sides);
+            if(sides[2]>=(sides[0]+sides[1])){
+                answer =2;
+            }
+            else {
+                answer =1;
+            }
+
+
+
+            return answer;
+        }
+    }
+}
